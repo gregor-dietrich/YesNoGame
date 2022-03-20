4158179ded74f529f7b883ce82970a93810f0ea4
gd <11874515+gregor-dietrich@users.noreply.github.com>
2022-03-20 11:45:35 +0100

    BTree

--- src/main/BTree.java
+++ src/main/BTree.java
@@ -12,14 +12,6 @@ public final class BTree {
         this.root = root;
     }
 
-    public Node getRoot() {
-        return root;
-    }
-
-    public void setRoot(final Node root) {
-        this.root = root;
-    }
-
     public void printPreOrder() {
         root.printPreOrder();
     }
@@ -30,5 +22,52 @@ public final class BTree {
 
     public void printInOrder() {
         root.printInOrder();
+    }
+
+    public void print() {
+        root.print();
+    }
+
+    public boolean isEmpty() {
+        return root == null;
+    }
+
+    public int countNodes() {
+        return countNodes(root);
+    }
+
+    private int countNodes(final Node node) {
+        if (node == null) return 0;
+        int count = 1;
+        count += countNodes(node.getLeft());
+        count += countNodes(node.getRight());
+        return count;
+    }
+
+    public boolean search(final int data) {
+        return search(root, data);
+    }
+
+    private boolean search(final Node node, final int data) {
+        if (node.getData() == data) return true;
+        if (node.getLeft() != null) if (search(node.getLeft(), data)) return true;
+        if (node.getRight() != null) return search(node.getRight(), data);
+        return false;
+    }
+
+    public Node getRoot() {
+        return root;
+    }
+
+    public void setRoot(final Node root) {
+        this.root = root;
+    }
+
+    public Node getCurrent() {
+        return currentNode;
+    }
+
+    public void setCurrent(final Node current) {
+        this.currentNode = current;
     }
 }
